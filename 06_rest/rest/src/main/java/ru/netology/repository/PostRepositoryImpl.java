75e3cc101042a2a2b6497e7f02e8e2de353999a4
Yaroslav Voitiuk <yaroslav.voitiuk@gmail.com>
2022-01-12 22:24:58 +0200

    bugs fixed

--- 06_rest/rest/src/main/java/ru/netology/repository/PostRepositoryImpl.java
+++ 06_rest/rest/src/main/java/ru/netology/repository/PostRepositoryImpl.java
@@ -24,19 +24,25 @@ public class PostRepositoryImpl implements PostRepository{
     }
 
     public List<Post> all() {
-        return new ArrayList<>(posts.values());
+        List<Post> postsList = new ArrayList<>();
+        for (Map.Entry<Long, Post> entry : posts.entrySet()) {
+            if(!entry.getValue().isRemoved())
+                postsList.add(entry.getValue());
+        }
+        return postsList;
     }
 
     public Optional<Post> getById(long id) {
+        if(posts.get(id).isRemoved()) throw new NotFoundException();
         return Optional.ofNullable(posts.get(id));
 
     }
 
     public Post save(Post post) {
-        if (post.getId() == 0 && !posts.containsKey(post.getId())) {
+        if (post.getId() == 0) {
             Post newPost = new Post(id.incrementAndGet(), post.getContent());
             posts.put(id.get(), newPost);
-        } else if (post.getId() != 0 && posts.containsKey(post.getId())) {
+        } else if (post.getId() != 0 && posts.containsKey(post.getId()) && !posts.get(post.getId()).isRemoved()) {
             posts.put(post.getId(), post);
         } else {
             throw new NotFoundException("no such posts found");
@@ -45,9 +51,8 @@ public class PostRepositoryImpl implements PostRepository{
     }
 
     public void removeById(long postId) {
-        if(posts.containsKey(postId)){
-            posts.remove(postId);
-            id.set(posts.size());
+        if(posts.containsKey(postId) && !posts.get(postId).isRemoved()){
+            posts.get(postId).setRemoved(true);
         }else throw new NotFoundException("no such posts found");
     }
 }
